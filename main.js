2c2c10baf01d1a6b63862624c5e08541c81b3693
Vincent De Gheyndt <vincent.degheyndt@gmail.com>
2020-02-13 10:17:53 +0100

    mise a jour publication

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var modernSlider = new Swiper('.swiper-container', {
+let modernSlider = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 100,
     centeredSlides: true,
@@ -55,6 +55,97 @@ TweenMax.to('.swiper-slide', 0, {
     scale: .95,
 })
 
+
+var $slider = $(".slider");
+var $slides = $slider.find(".slider-item");
+var $navPrev = $(".go-prev");
+var $navNext = $(".go-next");
+var slidesNum = $slides.length;
+var prevSlideID = null;
+var currentSlideID = 0;
+var isAnimating = false;
+var isAutoPlay = false;
+
+function init() {
+	TweenLite.set($slides, {
+		left: "-100%"
+	});
+	$navPrev.on("click", gotoPrevSlide);
+	$navNext.on("click", gotoNextSlide);
+	$startAutoplay.on("click", startAutoPlay);
+	$stopAutoplay.on("click", stopAutoPlay);
+	gotoSlide(0, 0);
+}
+
+function gotoPrevSlide() {
+	var slideToGo = currentSlideID - 1;
+	if (slideToGo <= -1) {
+		slideToGo = slidesNum - 1;
+	}
+	stopAutoPlay();
+	gotoSlide(slideToGo, 1, "prev");
+}
+
+function gotoNextSlide() {
+	var slideToGo = currentSlideID + 1;
+	if (slideToGo >= slidesNum) {
+		slideToGo = 0;
+	}
+	stopAutoPlay();
+	gotoSlide(slideToGo, 1, "next");
+}
+
+function gotoSlide(slideID, _time, _direction) {
+	if (!isAnimating) {
+		isAnimating = true;
+		prevSlideID = currentSlideID;
+		currentSlideID = slideID;
+		var $prevSlide = $slides.eq(prevSlideID);
+		var $currentSlide = $slides.eq(currentSlideID);
+		var time = 1;
+		if (_time !== null) {
+			time = _time;
+		}
+		var direction = "next";
+		if (_direction != null) {
+			direction = _direction;
+		}
+		if (direction == "next") {
+			TweenLite.to($prevSlide, time, {
+				left: "-100%"
+			});
+			TweenLite.fromTo($currentSlide, time, {
+				left: "100%"
+			}, {
+				left: "0"
+			});
+		} else {
+			TweenLite.to($prevSlide, time, {
+				left: "100%"
+			});
+			TweenLite.fromTo($currentSlide, time, {
+				left: "-100%"
+			}, {
+				left: "0"
+			});
+		}
+		TweenLite.delayedCall(time, function() {
+			isAnimating = false;
+		});
+	}
+}
+
+function play(){
+  gotoNextSlide();
+  TweenLite.delayedCall(4, play);
+}
+
+
+
+
+
+
+
 // url qui contiendra les repo
 const affichage = document.getElementById("api")
 
@@ -85,8 +176,4 @@ async function github(){
 
     });
 }
-
-
-github();
-
 
